fd50ce3b40eb19fd3a0f426fa1a13228bfbf9692
victhor hugo <victhor-hugo-11@hotmail.com>
2020-08-08 18:37:11 -0300

    gitignore config.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { config } from './config'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'filme';
+  title = config.api_key;
 }
